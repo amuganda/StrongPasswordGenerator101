dc488a918d50b0ef24d457aa72ba4d45dce2dc03
Ant M <amuganda@users.noreply.github.com>
2018-10-05 05:47:11 +0100

    Update Strong Password Generator.py

--- Strong Password Generator.py
+++ Strong Password Generator.py
@@ -7,10 +7,10 @@
 from secrets import choice
 import string
 
-generate = input('Number Of Passwords Required: ')
-generate = int(generate)
+pNumber = input('Number Of Passwords Required: ')
+pNumber = int(pNumber)
 
 N = 16
-for outputnum in range(generate):
+for outputnum in range(pNumber):
     print (''.join([choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(N)]))
 
